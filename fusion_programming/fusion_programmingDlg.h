a312404dea71cda021b617a03d7dd0856f83495a
chw1119 <chahyunwooa@naver.com>
2023-12-02 23:19:05 +0900

    초기 커밋.

---
+++ fusion_programming/fusion_programmingDlg.h
@@ -0,0 +1,33 @@
+﻿
+// fusion_programmingDlg.h: 헤더 파일
+//
+
+#pragma once
+
+
+// CfusionprogrammingDlg 대화 상자
+class CfusionprogrammingDlg : public CDialogEx
+{
+// 생성입니다.
+public:
+	CfusionprogrammingDlg(CWnd* pParent = nullptr);	// 표준 생성자입니다.
+
+// 대화 상자 데이터입니다.
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_FUSION_PROGRAMMING_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 지원입니다.
+
+
+// 구현입니다.
+protected:
+	HICON m_hIcon;
+
+	// 생성된 메시지 맵 함수
+	virtual BOOL OnInitDialog();
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+};
